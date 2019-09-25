0716b71d3277ce8392a547a61e1fab33bce84744
vagrant <vagrant@todolist>
2019-09-25 04:30:30 +0000

    Haciendo pruebas con UserController obteniendo datos

--- app/Http/Controllers/user/UserController.php
+++ app/Http/Controllers/user/UserController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers\user;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
@@ -14,7 +15,9 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        $users = User::all();
+
+        return $users;
     }
 
     /**
